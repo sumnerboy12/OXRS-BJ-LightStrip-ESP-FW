eb5c523208fb1d350bbcc04487789592ca86e428
chchben <sumnerboy12@users.noreply.github.com>
2025-04-03 16:14:27 +1300

    revert changes to reduce warning logs after Home Assistant PR was merged

--- src/main.cpp
+++ src/main.cpp
@@ -280,12 +280,12 @@ void publishHassDiscovery(LEDStrip * ledStrip)
   sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d %%}{{ value_json.state }}{%% endif %%}"), strip);
   json["stat_tpl"] = mqttTemplate;
 
-  sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d and value_json.brightness is defined %%}{{ value_json.brightness }}{%% endif %%}"), strip);
+  sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d %%}{{ value_json.brightness }}{%% endif %%}"), strip);
   json["bri_tpl"] = mqttTemplate;
 
   if (ledStrip->channels == 2 || ledStrip->channels == 5)
   {
-    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d and value_json.color_temp is defined %%}{{ value_json.color_temp }}{%% endif %%}"), strip);
+    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d %%}{{ value_json.color_temp }}{%% endif %%}"), strip);
     json["clr_temp_tpl"] = mqttTemplate;
 
     json["max_mirs"] = MAX_MIREDS;
@@ -294,11 +294,11 @@ void publishHassDiscovery(LEDStrip * ledStrip)
   
   if (ledStrip->channels >= 3)
   {
-    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d and value_json.color is defined %%}{{ value_json.color.r }}{%% endif %%}"), strip);
+    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d %%}{{ value_json.color.r }}{%% endif %%}"), strip);
     json["r_tpl"] = mqttTemplate;
-    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d and value_json.color is defined %%}{{ value_json.color.g }}{%% endif %%}"), strip);
+    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d %%}{{ value_json.color.g }}{%% endif %%}"), strip);
     json["g_tpl"] = mqttTemplate;
-    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d and value_json.color is defined %%}{{ value_json.color.b }}{%% endif %%}"), strip);
+    sprintf_P(mqttTemplate, PSTR("{%% if value_json.strip == %d %%}{{ value_json.color.b }}{%% endif %%}"), strip);
     json["b_tpl"] = mqttTemplate;
   }
 
@@ -312,34 +312,38 @@ void publishStripStatus(LEDStrip * ledStrip)
 
   json["strip"] = ledStrip->index + 1;
   json["state"] = ledStrip->state == LED_STATE_ON ? "on" : "off";
-  json["brightness"] = ledStrip->brightness;
 
-  switch (ledStrip->channels)
+  if (ledStrip->state == LED_STATE_ON)
   {
-    case 2:
-      json["color_temp"] = ledStrip->mired;
-      break;
+    json["brightness"] = ledStrip->brightness;
 
-    case 3:
-      json["color"]["r"] = ledStrip->color[0];
-      json["color"]["g"] = ledStrip->color[1];
-      json["color"]["b"] = ledStrip->color[2];
-      break;
+    switch (ledStrip->channels)
+    {
+      case 2:
+        json["color_temp"] = ledStrip->mired;
+        break;
 
-    case 4:
-      json["color"]["r"] = ledStrip->color[0];
-      json["color"]["g"] = ledStrip->color[1];
-      json["color"]["b"] = ledStrip->color[2];
-      json["color"]["w"] = ledStrip->color[3];
-    break;
-    
-    case 5:
-      json["color"]["r"] = ledStrip->color[0];
-      json["color"]["g"] = ledStrip->color[1];
-      json["color"]["b"] = ledStrip->color[2];
-      json["color"]["c"] = ledStrip->color[3];
-      json["color"]["w"] = ledStrip->color[4];
+      case 3:
+        json["color"]["r"] = ledStrip->color[0];
+        json["color"]["g"] = ledStrip->color[1];
+        json["color"]["b"] = ledStrip->color[2];
+        break;
+
+      case 4:
+        json["color"]["r"] = ledStrip->color[0];
+        json["color"]["g"] = ledStrip->color[1];
+        json["color"]["b"] = ledStrip->color[2];
+        json["color"]["w"] = ledStrip->color[3];
       break;
+      
+      case 5:
+        json["color"]["r"] = ledStrip->color[0];
+        json["color"]["g"] = ledStrip->color[1];
+        json["color"]["b"] = ledStrip->color[2];
+        json["color"]["c"] = ledStrip->color[3];
+        json["color"]["w"] = ledStrip->color[4];
+        break;
+    }
   }
 
   oxrs.publishStatus(json);
